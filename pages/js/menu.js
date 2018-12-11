d30074a95d27e7906609a21a4660d60cf6adc362
Jan Knappe <jan.knappe@tcd.ie>
2018-12-11 16:38:19 +0000

    w

--- pages/js/menu.js
+++ pages/js/menu.js
@@ -1,24 +1,3 @@
-$(document).ready(function(){
-    // References:
-    var $form = $('#myForm');
-    var $conf = $('#myConf');
-    var $subm = $('#mySubmit');
-    var $impt = $form.find(':input').not(':button, :submit, :reset, :hidden');
- // Submit function:
-    $form.submit(function(){
-        $.post($(this).attr('action'), $(this).serialize(), function(response){
-      // On success, clear all inputs;
-            $impt.val('').attr('value','').removeAttr('checked').removeAttr('selected');
-   // Write a confirmation message:
-            $conf.html("Submitted!");
-   // Disable the submit button:
-            $subm.prop('disabled', true);
-        },'json');
-        return false;
-    });
-});
-
-
   var category = {
   Expense: ["Food", "Purchases", "Transportation", "Entertainment", "Travel", "Fixed", "Others"],
   Income: ["Salary", "Cash", "Gifts", "Investment", "Sales", "Others"]
