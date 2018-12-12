984391cc3ef72a6ac373f97cbb1419c3fdae0040
Jan Knappe <jan.knappe@tcd.ie>
2018-12-12 14:20:07 +0000

    Update menu.js

--- pages/js/menu.js
+++ pages/js/menu.js
@@ -44,20 +44,23 @@ function start(){
 }
 
 function typeChange(value) {	
-	console.log(value);
+
+console.log(value);
     makeSubmenu(value);
 	
 	var type = value;
-	console.log(type);
+console.log(type);
 	    if (type == "Income") value = 1;
 		else{
 			value = 2
 		}
-	console.log(type);
-	console.log(value);
+console.log(type);
+console.log(value);
     var catOptions = "";
+console.log(catOptions);	
     for(categoryId in subcategory[value]) {
       catOptions+="<option>"+subcategory[value][categoryId]+"</option>";
     }
+console.log(catOptions);
     document.getElementById("subCategorySelect").innerHTML = catOptions;
 }
